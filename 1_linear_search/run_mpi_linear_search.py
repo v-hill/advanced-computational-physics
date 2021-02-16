a3e5d894299324330d204d047851578325e12a68
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-02-16 23:31:15 +0000

    Update run_mpi_linear_search.py

--- 1_linear_search/run_mpi_linear_search.py
+++ 1_linear_search/run_mpi_linear_search.py
@@ -2,12 +2,12 @@
 This scripts implements the nearest neighbour linear search algorithm in 
 parallel using MPI.
 
-Example run command, using default 1000 generated points:
+Example run command, using default 1000 generated points on 4 CPU cores:
         mpiexec -n 4 python .\run_mpi_linear_search.py
         
 Example run command to scan through 100 to 10,000 points repeating each 
-execution 2 times:
-        mpiexec -n 2 python .\run_mpi_linear_search.py --num_points_scan -r 2
+execution 3 times:
+        mpiexec -n 4 python .\run_mpi_linear_search.py --num_points_scan -r 3
 """
 
 # Python libraries
@@ -15,6 +15,8 @@ from mpi4py import MPI
 import argparse
 import numpy as np
 import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 
 # Code from local files
 from linear_search.boids import World, Boids
@@ -34,7 +36,6 @@ def main(mpi, num_points, max_neighbour_dist, scan):
     world = World(world_size)
     boids = Boids(num_points, world, max_neighbour_dist)
     boids.generate_boids()
-    
     
     if mpi['rank'] == 0 and not scan:
         print(f'Running on {mpi["size"]} cores...')
@@ -68,7 +69,6 @@ def main(mpi, num_points, max_neighbour_dist, scan):
         new_members = []
         for data in data_gathered:
             new_members += data
-        
         boids.members = new_members
         
         # End timer and print elasped time
@@ -78,6 +78,52 @@ def main(mpi, num_points, max_neighbour_dist, scan):
             return [num_points, elapsed*1000]
         else:
             return f'\t {elapsed*1000:0.1f} ms'
+
+def make_df(repeats, results, disable_averaging):
+    if args.repeats==0 or disable_averaging==True:
+        columns = ['Number of points', 
+                   ('Execution time for Manhattan distance using '
+                   f'MPI on {mpi["size"]} cores (in ms)')]
+    else:
+        columns = ['Number of points', 
+                   ('Execution time using '
+                   f'MPI on {mpi["size"]} cores, average of '
+                   f'{args.repeats} repeats (in ms)')]   
+    df = pd.DataFrame(results, columns=columns)
+    if not disable_averaging:
+        df = df.groupby('Number of points').mean().reset_index()
+    return df
+
+def save_df(df, filename):
+    date_time = datetime.datetime.now().strftime('_%Y_%m_%d__%H_%M_%S')
+    filename = filename + date_time + '.csv'
+    df.to_csv(filename, index=False)
+
+def plot_results(df, filename, title, save=True):
+    fig = plt.figure(figsize=(4, 4), dpi=300)
+    ax = fig.add_subplot(111)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    plt.xlim(50, 25000)
+    #plt.ylim(1, 250000)
+    
+    plt.title(title, fontsize=11)
+    plt.xlabel("Number of points", fontsize=11)
+    plt.ylabel("Time in ms", fontsize=11)
+    
+    plt.plot(df.iloc[:,0], df.iloc[:,1], 'o--', 
+             linewidth=0.5, markersize=4, 
+             label=f'MPI {mpi["size"]} cores')
+
+    plt.grid(b=True, which='major', color=(0.5, 0.5, 0.5), linestyle='--', linewidth=0.3)
+    ax.legend()
+    
+    if save:
+        date_time = datetime.datetime.now().strftime('_%Y_%m_%d__%H_%M_%S')
+        filename = filename + date_time + '.png'
+        plt.savefig(filename, dpi=fig.dpi, bbox_inches = 'tight')
+    else:
+        plt.show()
 
 # ------------------------------------ main -----------------------------------
 
@@ -106,6 +152,13 @@ if __name__ == '__main__':
                         'value', 
                         type=int,
                         default = 1)
+    
+    parser.add_argument('--disable_averaging', 
+                        default=False, action='store_true',
+                        help=('By default, when multiple repeat measurements '
+                              'are made the timings are average for each '
+                              'num_points value. Use this option to disable '
+                              'averaging and save the full results'))
     args = parser.parse_args()
     
     mpi = mpi_setup()
@@ -133,17 +186,13 @@ if __name__ == '__main__':
             results.append(res)
         
         if mpi['rank'] == 0:
-            if args.repeats==0:
-                columns = ['Number of points', 
-                           ('Execution time for Manhattan distance using '
-                           f'MPI on {mpi["size"]} cores (in ms)')]
-            else:
-                columns = ['Number of points', 
-                           ('Execution time using '
-                           f'MPI on {mpi["size"]} cores, average of '
-                           f'{args.repeats} repeats (in ms)')]   
+            data_filename = 'linear_search_MPI_timings'
+            plot_filename = 'linear_search_MPI_timings_plot'
+            plot_title =  ('NNS linear search algorithm using MPI on '
+                          f'{mpi["size"]} cores')
             
-            df = pd.DataFrame(results, columns=columns)
-            df = df.groupby('Number of points').mean().reset_index()
+            df = make_df(args.repeats, results, args.disable_averaging)
+            save_df(df, data_filename)
+            plot_results(df, plot_filename, plot_title)
             print(df)
             
