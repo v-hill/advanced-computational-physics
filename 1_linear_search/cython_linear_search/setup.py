5e92fbfcccd9526e9d693f8fbf147cb691a5274d
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-02-19 23:40:24 +0000

    Update setup.py

--- 1_linear_search/cython_linear_search/setup.py
+++ 1_linear_search/cython_linear_search/setup.py
@@ -5,17 +5,18 @@ To build this file run the command from within anaconda prompt:
     python setup.py build_ext --inplace --compiler=msvc
 """
 
+# Python libraries
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-
 import numpy
+
 setup(
-      name        = 'linear_search',
+      name        = 'cython_linear_search',
       cmdclass    = {'build_ext':build_ext},
       include_dirs=[numpy.get_include()],
-      ext_modules = [Extension("openmp_linear_search",["openmp_linear_search.pyx"], 
-                               extra_compile_args=['-fopenmp'], 
-                               extra_link_args=['-fopenmp'])]
+      ext_modules = [Extension("cython_linear_search",["cython_linear_search.pyx"], 
+                               extra_compile_args=['/openmp'], 
+                               extra_link_args=['/openmp'])]
       )
 
