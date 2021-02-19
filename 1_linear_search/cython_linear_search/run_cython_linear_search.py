7d1103045e0a14e52196c04004695bdbafdeb892
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-02-19 23:40:10 +0000

    Update and rename run_openmp_linear_search.py to run_cython_linear_search.py

--- 1_linear_search/cython_linear_search/run_cython_linear_search.py
+++ 1_linear_search/cython_linear_search/run_cython_linear_search.py
@@ -1,17 +1,20 @@
-import openmp_linear_search
-import generate_values
+# Python libraries
 import time
+
+# Code from local files
+import cython_linear_search
+import generate_values
 
 num_points = 1000
 max_neighbour_dist = 100
 world_size = [0, 1000, 0, 1000]
 positions = generate_values.random(num_points, world_size)
 
-boids = openmp_linear_search.setup(num_points, max_neighbour_dist, positions)
+boids = cython_linear_search.setup(num_points, max_neighbour_dist, positions)
 
 START = time.time()
 
-openmp_linear_search.main(boids)
+cython_linear_search.main(boids)
 
 elapsed = time.time() - START
 print(f'\t {elapsed*1000:0.1f} ms')
