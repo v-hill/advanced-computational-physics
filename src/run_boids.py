77ee50e4be2d246a0fa708c7dae82fcf9d7845da
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-03-01 04:22:41 +0000

    Update run_boids.py

--- src/run_boids.py
+++ src/run_boids.py
@@ -1,3 +1,75 @@
+"""
+This script contains a command line interface for running the full boids 
+animation. This full list of possible user arguments is detailed below.
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+Simulation options:
+  number_of_boids       Number of boids in the simulation
+                        (type: int)
+  --still_image         Instead of an animation, show a single still frame
+                        (default: False)
+  --boid_distribution [{random,lattice,lattice_with_noise}]
+                        Define how the boids are initally arranged within the world
+                        (choices: random, lattice, lattice_with_noise) 
+                        (default: lattice)
+
+Boid world options:
+  -ww , --world_width   Width of the boids plot in pixels
+                        (default: 1000) (type: int)
+  -wh , --world_height
+                        Height of the boids plot in pixels
+                        (default: 1000) (type: int)
+
+Output plot options:
+  -tw , --triangle_width
+                        Width of the boid triangles in pixels
+                        (default: 8) (type: int)
+  -th , --triangle_height
+                        Height of the boid triangles in pixels
+                        (default: 12) (type: int)
+  -dpl , --direction_line_len
+                        Length of the boid direction arrow pointers in pixels
+                        (default: 50) (type: int)
+  -nc , --num_colours   Number of possible colours a boid can be
+                        (default: 4) (type: int)
+  --no_boid_colours     Do not plot boids with different colour values
+                        (default: False)
+  --no_border           Do not plot a border around the boid world
+                        (default: False)
+  -bs , --border_size   Size of border around boids world in pixels
+                        (default: 50) (type: int)
+  --background_colour [{black,white}]
+                        Set the background colour of the output plot
+                        (choices: black, white)
+  --save SAVE           Save the output, given a filename
+                        (type: str)
+
+Boid options:
+  -vmax , --max_speed   Boid max speed in pixels per timestep
+                        (default: 2) (type: int)
+  -fov , --field_of_view
+                        Boid field of view, as a fraction of a full circle.
+                        e.g. 0.5 gives π radians fov
+                        (default: 0.66) (type: float_with_range)
+  -vd , --vision_distance
+                        Limit how far away a neighbouring boid can be in order 
+                        to stil be considered a neighbour
+                        (default: 200) (type: int)
+  -sz , --safety_zone   Set how close another boid can be before avoidance 
+                        behaviour occurs
+                        (default: 20) (type: int)
+  -ali , --alignment_perception
+                        Strength of boid alignment to neighbours travel directions
+                        (default: 0.08) (type: float)
+  -coh , --cohesion_perception
+                        Strength of boid cohesion to neighbours
+                        (default: 0.008) (type: float)
+  -sep , --seperation_perception
+                        Strength of boid seperation from neighbours
+                        (default: 0.25) (type: float)
+"""
 
 # ---------------------------------- Imports ----------------------------------
 
@@ -6,9 +78,11 @@ import argparse
 from math import pi
 
 # Repo module imports
-from settings import world_options, plotting_options, boids_options
-from boids import World, Boids
-import plotting
+from boids_core.settings import world_options, plotting_options, boids_options
+from boids_core.boids import World, Boids
+from boids_core import plotting 
+
+# -----------------------------------------------------------------------------  
 
 def float_with_range(x):
     try:
@@ -20,6 +94,24 @@ def float_with_range(x):
     return x
 
 def set_options(world_options, plotting_options, boids_options):
+    """
+    Function using argparse to parse all user parameters.
+
+    Parameters
+    ----------
+    world_options : dict
+        default options dictionary
+    plotting_options : dict
+        default options dictionary
+    boids_options : dict
+        default options dictionary
+
+    Returns
+    -------
+    options : dict
+        All simulation options
+
+    """
     simulation_options = {}
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                      description='Boids run program')
@@ -216,6 +308,8 @@ def set_options(world_options, plotting_options, boids_options):
                **simulation_options}
     return options
 
+# -----------------------------------------------------------------------------  
+
 def main(options):
     # Setup world
     WORLD_SIZE = [0, options['world_width'], 
@@ -224,6 +318,7 @@ def main(options):
     cmap = plotting.ColourMap(options)
     plot = plotting.Plotter(options, world)
     num_boids = options['number_of_boids']
+    print_fps_to_console = True
     boids = Boids(num_boids, world, options)
     boids.generate_boids(options, distribution=options['boid_distribution'])
     
@@ -235,7 +330,7 @@ def main(options):
         for i in range(num_boids):
             a = boids.members[i]
             # a.update_boid(boids.positions, boids.velocities, world)
-            if a.index%8==0:
+            if a.index%32==0:
                 plot.plot_neighbours(a, boids.positions)
         plot.plot_boids(boids, cmap)
         plot.display()
@@ -243,8 +338,22 @@ def main(options):
             plot.save(plotting_options['save_filename'])
     
     else:
+        def plot_func(boids):
+            boids.triangulate_boids()
+            boids.make_neighbourhoods()
+            for i in range(num_boids):
+                a = boids.members[i]
+                a.update_boid(boids.positions, boids.velocities, world)
+                if a.index%50==0:
+                    plot.plot_neighbours(a, boids.positions)
+            return boids
+        
         print("\nPlotting animation...")
+        plot = plotting.Plotter(options, world)
+        plot.animation(boids, plot_func, cmap, 
+                       verbose=print_fps_to_console, print_fps=48)
 
+# ----------------------------------- Main ------------------------------------
 
 if __name__ == '__main__':
     options = set_options(world_options, plotting_options, boids_options)
